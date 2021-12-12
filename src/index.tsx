08daa2cca9c89e87a2ad7569c82d78337ca3904f
fireclaws9 <41904540+fireclaws9@users.noreply.github.com>
2021-12-12 16:26:03 +0800

    Classic Website Template

---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import PageBody from "./body/page_body";
+
+ReactDOM.render(<React.StrictMode><PageBody/></React.StrictMode>, document.body);
